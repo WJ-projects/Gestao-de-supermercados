dab682e80103af36ab3cfd3cdcb5b7a984cbd0a3
Wellyngton-douglas <wellyngtond159@gmail.com>
2021-04-25 01:42:45 -0300

    setando class endereço no painel do django-admin

--- GDS/admin.py
+++ GDS/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Cliente
+from .models import Cliente, Endereco
 
 # Register your models here.
 
-admin.site.register(Cliente)
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nome", "cpf", "email_cliente", "Nome_e_numero_da_casa",)
+
+@admin.register(Endereco)
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ("nome_rua", "numero_casa", "bairro", "complemento", "cep",)
